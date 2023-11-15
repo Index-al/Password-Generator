086eb429371acef0695128d0e0cd7cfb90dbc089
Indexal <148807519+Index-al@users.noreply.github.com>
2023-11-15 14:04:45 -0600

    Consolidated the logic into the generatePassword function instead of having a bunch of event listeners

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Create an array to store possible characters
-var possibleChars = [];
-var passwordLength = 0;
-
 // << LOGIC >>
 // When the user clicks on the button, ask a series of questions
 // First question: Enter a # between 8 and 128
@@ -26,102 +22,79 @@ var passwordLength = 0;
 // Then display the generated password on the page
 // << END LOGIC >>
 
-// Welcome the user to the password generator and explain how to use it
-generateBtn.addEventListener("click", function () {
+function generatePassword() {
+  // Welcome the user and explain how to use the password generator
   alert("Welcome to the password generator! Please answer the following questions to generate your password. If prompted for a yes or no answer, click OK for yes and Cancel for no. Click OK to begin!");
 
-  // Prompt the user for password length
-  passwordLength = prompt("Enter your password length (8-128 characters): ");
-
-  while (passwordLength === null || isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    if (passwordLength === null) {
-      // Close the dialog box if user clicks cancel
-      break;
+    // Reset options
+    possibleChars = [];
+    passwordLength = null;
+  
+    // Prompt the user for password length
+    passwordLength = prompt("Enter your password length (8-128 characters): ");
+  
+    while (passwordLength === null || isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+      if (passwordLength === null) {
+        // Close the dialog box if the user clicks cancel
+        break;
+      }
+      passwordLength = prompt("Invalid input. Enter a number between 8 and 128: ");
     }
-    passwordLength = prompt("Invalid input. Enter a number between 8 and 128: ");
+  
+    // If the user enters a valid number, log it to the console
+    if (passwordLength !== null) {
+      console.log("Valid input:", passwordLength);
+    } else {
+      console.log("Prompt canceled or closed.");
+      return; // Exit the function if the user cancels or closes the prompt
+    }
+  
+    // Prompt the user for lowercase letters in the password
+    let lowercase = confirm("Would you like lowercase letters in your password?");
+    if (lowercase) {
+      possibleChars = possibleChars.concat("abcdefghijkmnopqrstuvwxyz".split(""));
+    }
+  
+    // Prompt the user for uppercase letters in the password
+    let uppercase = confirm("Would you like uppercase letters in your password?");
+    if (uppercase) {
+      possibleChars = possibleChars.concat("ABCDEFGHJKLMNPQRSTUVWXYZ".split(""));
+    }
+  
+    // Prompt the user for numbers in the password
+    let numbers = confirm("Would you like numbers in your password?");
+    if (numbers) {
+      possibleChars = possibleChars.concat("0123456789".split(""));
+    }
+  
+    // Prompt the user for special characters in the password
+    let special = confirm("Would you like special characters in your password?");
+    if (special) {
+      possibleChars = possibleChars.concat("!@#$%^&*()_+-=[]{};':\",./<>?".split(""));
+    }
+  
+    // Create an empty string to store the password
+    var password = "";
+  
+    // Loop through the array of possible characters
+    for (var i = 0; i < passwordLength; i++) {
+      // Generate a random number between 0 and the length of the array
+      var randomIndex = Math.floor(Math.random() * possibleChars.length);
+      // Add the character at the random index to the password string
+      password += possibleChars[randomIndex];
+    }
+  
+    // Return the password string
+    return password;
   }
-  // If the user enters a valid number, log it to the console
-  if (passwordLength !== null) {
-    console.log("Valid input:", passwordLength);
-  } else {
-    console.log("Prompt canceled or closed.");
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
   }
-});
-
-// Prompt the user for lowercase letters in password
-generateBtn.addEventListener("click", function () {
-  let lowercase = confirm("Would you like lowercase letters in your password?");
-  // If the user selects lowercase letters, add them to the array
-  if (lowercase) {
-    possibleChars = possibleChars.concat("abcdefghijkmnopqrstuvwxyz".split("")); // Ambiguous characters removed
-  }
-  console.log("lowercase:", lowercase);
-  return lowercase;
-});
-
-// Prompt the user for uppercase letters in password
-generateBtn.addEventListener("click", function () {
-  let uppercase = confirm("Would you like uppercase letters in your password?");
-  // If the user selects uppercase letters, add them to the array
-  if (uppercase) {
-    possibleChars = possibleChars.concat("ABCDEFGHJKLMNPQRSTUVWXYZ".split("")); // Ambiguous characters removed
-  }
-  console.log("uppercase:", uppercase);
-  return uppercase;
-});
-
-// Prompt the user for numbers in password
-generateBtn.addEventListener("click", function () {
-  let numbers = confirm("Would you like numbers in your password?");
-  // If the user selects numbers, add them to the array
-  if (numbers) {
-    possibleChars = possibleChars.concat("0123456789".split(""));
-  }
-  console.log("numbers:", numbers);
-  return numbers;
-});
-
-// Prompt the user for special characters in password
-generateBtn.addEventListener("click", function () {
-  let special = confirm("Would you like special characters in your password?");
-  // If the user selects special characters, add them to the array
-  if (special) {
-    possibleChars = possibleChars.concat("!@#$%^&*()_+-=[]{};':\",./<>?".split(""));
-  }
-  console.log("special:", special);
-  return special;
-});
-
-function generatePassword() {
-  // Create an empty string to store the password
-  var password = "";
-  // Loop through the array of possible characters
-  for (var i = 0; i < passwordLength; i++) {
-    // Generate a random number between 0 and the length of the array
-    var randomIndex = Math.floor(Math.random() * possibleChars.length);
-    // Add the character at the random index to the password string
-    password += possibleChars[randomIndex];
-  }
-  // Return the password string
-  return password;
-}
-
-// Write password to the #password input
-function writePassword() {
-  // Generate the password
-  var password = generatePassword();
-
-  // Reset options
-  possibleChars = [];
-  passwordLength = null;
-
-  // Update the password input field
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);  
